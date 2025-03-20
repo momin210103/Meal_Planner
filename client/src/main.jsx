3c9058d541559b9621e3abff01c5b878f9bc75b6
momin210103 <momincse13@gmail.com>
2025-03-20 23:14:29 +0600

    changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, Routes,Route } from "react-router";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from './App.jsx';
 import Home from './Pages/home/Home.jsx';
-
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route element = {<App/>}>
-      <Route path="/" element ={<Home/>}/>
-      <Route path="/about" element ={<div>About</div>}/>
-      <Route path="/contact" element ={<div>Contact</div>}/>
-      <Route path="/login" element ={<div>Login</div>}/>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<div>About</div>} />
+        <Route path="contact" element={<div>Contact</div>} />
+        <Route path="login" element={<div>Login</div>} />
       </Route>
     </Routes>
   </BrowserRouter>
-)
+);

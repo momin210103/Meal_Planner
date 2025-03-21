c068d09c60bbd25f834b6192dbf8f3ce34f5ce0d
momin210103 <momincse13@gmail.com>
2025-03-21 12:24:22 +0600

    Login button hover

--- client/src/Pages/home/Hero.jsx
+++ client/src/Pages/home/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const Hero = () => {
     return (
         <section className='md:flex'>
-            <div className='pt-24 md:md:pt-48 text-center space-x-8 md:w-1/2 mx-auto text-black font-bold lg:leading-snug'>
+            <div className='bg-[#d4cec8] rounded-2xl pt-24 md:md:pt-48 text-center space-x-8 md:w-1/2 mx-auto text-black font-bold lg:leading-snug'>
                 <h1 className='text-4xl lg:text-6xl md:pt-48'>Welcome To Our MealPlaner</h1>
                 <p>Smart Plan Save Time</p>
                 <Link to="/login">
--- client/src/Pages/login/LoginPage.jsx
+++ client/src/Pages/login/LoginPage.jsx
@@ -46,7 +46,7 @@ const LoginPage = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 cursor-pointer"
           >
             Login
           </button>

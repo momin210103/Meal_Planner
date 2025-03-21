4f61039ea7bd65b1ca6f7293226881e92bd38c61
momin210103 <momincse13@gmail.com>
2025-03-21 12:14:15 +0600

    connet login page to get started button

--- client/src/Pages/home/Hero.jsx
+++ client/src/Pages/home/Hero.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import HeroImg from '../../assets/HeroImg.png'
+import { Link } from 'react-router';
 const Hero = () => {
-    const loginpage = ()=>{
-        
-    }
     return (
         <section className='md:flex'>
             <div className='pt-24 md:md:pt-48 text-center space-x-8 md:w-1/2 mx-auto text-black font-bold lg:leading-snug'>
                 <h1 className='text-4xl lg:text-6xl md:pt-48'>Welcome To Our MealPlaner</h1>
                 <p>Smart Plan Save Time</p>
-                <button className='cursor-pointer bg-green-700 rounded-2xl p-2 my-2' onClick={loginpage}>Get Started </button>
+                <Link to="/login">
+        <button className="bg-green-900 text-white px-6 py-2 my-2 rounded-lg font-semibold hover:bg-gray-100 hover:text-black transition duration-300 cursor-pointer">
+          Get Started
+        </button>
+      </Link>
             </div>
             <div className='relative bg-cover bg-center text-white h-screen w-full md:w-1/2 rounded-2xl' style={{backgroundImage:`url(${HeroImg})`}}>
             </div>

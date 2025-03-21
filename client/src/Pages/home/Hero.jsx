d5aa39d4145ae917d63d6e06f550be2ee0858db2
momin210103 <momincse13@gmail.com>
2025-03-21 11:01:03 +0600

    Changed Hero image size changed

--- client/src/Pages/home/Hero.jsx
+++ client/src/Pages/home/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HeroImg from '../../assets/HeroImg.png'
-import HeroImg2 from '../../assets/Hero2.jpg'
 const Hero = () => {
     return (
-        <section className='h-screen relative bg-cover bg-center text-white' style={{backgroundImage:`url(${HeroImg})`}}>
-            <div className='pt-24 md:md:pt-48 text-center space-x-8 md:w-1/2 mx-auto text-white font-bold lg:leading-snug'>
+        <section className='md:flex'>
+            <div className='pt-24 md:md:pt-48 text-center space-x-8 md:w-1/2 mx-auto text-black font-bold lg:leading-snug'>
                 <h1 className='text-4xl lg:text-6xl md:pt-48'>Welcome To Our MealPlaner</h1>
                 <p>Smart Plan Save Time</p>
+            </div>
+            <div className='relative bg-cover bg-center text-white h-screen w-full md:w-1/2 rounded-2xl' style={{backgroundImage:`url(${HeroImg})`}}>
             </div>
         </section>
     );

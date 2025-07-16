3867d207b491ab11c92fafd213890415a3d4253f
momin210103 <momincse13@gmail.com>
2025-07-16 09:37:39 +0600

    import fiarrow

--- client/src/Pages/login/LoginPage.jsx
+++ client/src/Pages/login/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import { Link, useNavigate } from 'react-router';
 import axios from 'axios';
+import { FiArrowLeft } from 'react-icons/fi';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
